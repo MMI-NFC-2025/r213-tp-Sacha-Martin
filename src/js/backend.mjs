02c5d0a7130dc602ebfb2aa3b2f278f4d6f7a574
Sacha-Martin <sacha.martin03@edu.univ-fcomte.fr>
2026-02-27 11:49:25 +0100

    commit fin tp

---
+++ src/js/backend.mjs
@@ -0,0 +1,57 @@
+import PocketBase from 'pocketbase';
+
+const db = new PocketBase('http://127.0.0.1:8090/');
+
+export async function getOffres() {
+    try {
+        let data = await db.collection('maison').getFullList({
+            sort: '-created',
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
+
+export async function getImageUrl(record, recordImage) {
+    return db.files.getURL(record, recordImage);
+}
+
+export async function getOffre(id) {
+    try {
+        const data = await db.collection('maison').getOne(id);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la maison', error);
+        return {};
+    }
+}
+
+export async function getNom(nomMaison) {
+    try {
+        const data = await db.collection('maison').getFullList({
+            filter: `nomMaison = "${nomMaison}"`
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en créant la maison', error);
+        return null;
+    }
+}
+
+export async function addOffre(house) {
+    try {
+        await db.collection('maison').create(house);
+        return {
+            success: true,
+            message: 'Offre ajoutée avec succès'
+        };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant la maison', error);
+        return {
+            success: false,
+            message: 'Une erreur est survenue en ajoutant la maison'
+        };
+    }
+}
